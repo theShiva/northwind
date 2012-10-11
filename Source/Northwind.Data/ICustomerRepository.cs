99262a13b2fa209961ce14ff86bcab4673d7b3f0
shiva manjunath <hey@shiva.io>
2012-10-11 00:05:49 -0700

    Added Northwind.Data project for Data Contracts. Also added 2 empty repository interfaces.

---
+++ Source/Northwind.Data/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Northwind.Data.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        //TODO: come up with public contracts
+    }
+}
---
+++ Source/Northwind.Data/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Northwind.Data.Interfaces
+{
+    public interface EmployeeRepository
+    {
+        //TODO: come up with public contracts
+    }
+}
